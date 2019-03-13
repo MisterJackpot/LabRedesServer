096c96d068751d4a5b3895296ff1c1a138cc5dd7
gabriel.moraes <gabriel.loff@acad.pucrs.br>
2019-03-13 13:11:11 -0300

    first commit

---
+++ src/TCPServer.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+
+public class TCPServer {
+
+    public final static int FILE_SIZE = 6022386;
+    public final static String FILE_TO_RECEIVED = "assets/source-downloaded.pdf";
+
+    public static void main (String args[]){
+
+        try {
+            ServerSocket server = new ServerSocket(3000);
+
+            InetAddress inet = server.getInetAddress();
+            System.out.println("Porta = "+server.getLocalPort());
+
+            while(true) {
+                System.out.println("Esperando...");
+
+                Socket cliente = server.accept();
+                System.out.println("Client " + cliente.getInetAddress().getHostAddress() + " connected on " + server.getLocalPort());
+
+                byte [] mybytearray  = new byte [FILE_SIZE];
+                InputStream in = cliente.getInputStream();
+                FileOutputStream fos = new FileOutputStream(FILE_TO_RECEIVED);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                int bytesRead = in.read(mybytearray,0,mybytearray.length);
+                int current = bytesRead;
+
+                do {
+                    bytesRead =
+                            in.read(mybytearray, current, (mybytearray.length-current));
+                    if(bytesRead >= 0) current += bytesRead;
+                } while(bytesRead > -1);
+
+                bos.write(mybytearray, 0 , current);
+                bos.flush();
+
+                System.out.println("File " + FILE_TO_RECEIVED
+                        + " downloaded (" + current + " bytes read)");
+
+
+
+                fos.close();
+                bos.close();
+                cliente.close();
+                System.out.println("Conexão Encerrada");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
